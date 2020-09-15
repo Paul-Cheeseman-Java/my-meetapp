d92711231377067d6789a98b5941660524b6788c
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2020-09-15 20:23:10 +0100

    DLS update #3

--- src/main/java/com/meetapp/meetings/model/Meeting.java
+++ src/main/java/com/meetapp/meetings/model/Meeting.java
@@ -188,7 +188,7 @@ public class Meeting  implements Comparable<Meeting> {
 		
 		//Daylight saving hack
 		//if date before 25/10/20 do below, else don't
-		LocalDate DSTSwitch = LocalDate.of(2020, 10, 25);
+		LocalDate DSTSwitch = LocalDate.of(2020, 10, 26);
 		LocalTime localTime = time.toLocalTime();
 		if (localDate.isAfter(DSTSwitch)) {
 			localTime = time.toLocalTime().minusHours(1);			
